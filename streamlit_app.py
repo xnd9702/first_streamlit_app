b8e2b0b953ce2dc55689ff8dbd5956db205c1f8d
xnd9702 <156121596+xnd9702@users.noreply.github.com>
2024-01-10 22:08:49 +0530

    Update streamlit_app.py
    adding text lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy  Diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
