285734105a64a0fba65191cc367ae9ef5a577a9f
xnd9702 <156121596+xnd9702@users.noreply.github.com>
2024-01-10 22:43:18 +0530

    Update streamlit_app.py
    adding names for the widget

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.title('My Mom\'s New Healthy  Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
