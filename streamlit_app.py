d7f858cefd75add032be572f2cc8fa59fdeef612
xnd9702 <156121596+xnd9702@users.noreply.github.com>
2024-01-14 03:53:37 +0530

    Update streamlit_app.py
    bring in another Python package library called requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.title('My Mom\'s New Healthy  Diner')
